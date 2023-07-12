76b8b932b9f87c753832c182d740373660aec5b6
DerekHan <donglak.han@gmail.com>
2023-07-12 12:27:30 -0500

    first commit

---
+++ components/Hero.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Hero = () => {
+  return <div>Hero</div>;
+};
+
+export default Hero;
+
+/** create components folder outside of app folder
+ * new Next13
+ * only related with route will go in app folder
+ */
