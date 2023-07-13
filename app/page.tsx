b04b24a02eb65fdc89a28ddf7b10609f5956fddd
DerekHan <donglak.han@gmail.com>
2023-07-13 10:51:09 -0500

    update param to search cars

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,15 @@ import Image from "next/image";
 
 import { fetchCars } from "@/utils";
 
-export default async function Home() {
-  const allCars = await fetchCars();
-  // console.log(allCars); Not working. cause everything is server side rendering in Next 13.
+export default async function Home({ searchParams }) {
+  const allCars = await fetchCars({
+    manufacturer: searchParams.manufacturer || "",
+    year: searchParams.year || 2022,
+    fuel: searchParams.fuel || "",
+    limit: searchParams.limit || 10,
+    model: searchParams.model || "",
+  });
+
   const isDataEmpty = !Array.isArray(allCars) || allCars.length < 1 || !allCars;
 
   return (
