386fd6c3a2d2c22f20be6a80236a394fd4b95371
DerekHan <donglak.han@gmail.com>
2023-07-12 22:06:44 -0500

    Added CarCard with Rent price

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
-import { CustomFilter, Hero, SearchBar } from "@/components";
-import { fetchCars } from "@/utils";
+import { CarCard, CustomFilter, Hero, SearchBar } from "@/components";
 import Image from "next/image";
+
+import { fetchCars } from "@/utils";
 
 export default async function Home() {
   const allCars = await fetchCars();
@@ -27,10 +28,16 @@ export default async function Home() {
         </div>
 
         {!isDataEmpty ? (
-          <section>WE HAVE CARS</section>
+          <section>
+            <div className="home__cars-wrapper">
+              {allCars?.map((car) => (
+                <CarCard car={car} />
+              ))}
+            </div>
+          </section>
         ) : (
-          <div>
-            <h2>Oops, no results</h2>
+          <div className="home__error-container">
+            <h2 className="text-black text-xl font-bold">Oops, no results</h2>
             <p>{allCars?.message}</p>
           </div>
         )}
